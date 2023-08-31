a33e8a780892b0adf3746eebf67e12bf61f343f0
cormacwit <cormackfarrell@gmail.com>
2023-08-31 07:00:45 +0100

    Completed optional lab

--- movies-api/index.js
+++ movies-api/index.js
@@ -7,6 +7,8 @@ import genresRouter from './api/genres';
 import usersRouter from './api/users';
 import session from 'express-session';
 import authenticate from './authenticate';
+import https from 'https';
+import fs from 'fs';
 
 dotenv.config();
 
@@ -17,6 +19,15 @@ const errHandler = (err, req, res) => {
   res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
 };
 
+const checkIfSecure = (req, res, next)=>{
+  if(req.secure){
+    //All Good - call next middleware
+    return next();
+  }
+  //insecure, redirect
+  res.redirect('https://' + req.hostname + ":8081" + req.url); // express 4.x
+}
+
 const app = express();
 
 app.use(session({
@@ -26,7 +37,7 @@ app.use(session({
 }));
 
 const port = process.env.PORT;
-
+app.all('*', checkIfSecure); 
 app.use(express.json());
 app.use('/api/genres', genresRouter);
 app.use('/api/users', usersRouter);
@@ -35,5 +46,9 @@ app.use(errHandler);
 
 
 app.listen(port, () => {
-  console.info(`Server running at http://localhost:${port}/`);
+  console.info(`Server running at ${port}`);
 });
+
+tlsServer.listen(8081,()=>{
+  console.log('Secure server is listening on port 8081')
+  })
